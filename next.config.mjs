428cc0208775cd8d2c9a5f41df1fff4b2dae9733
André Castro <andre@visie.com.br>
2024-11-27 14:46:41 -0300

    Voltando para antes de quebrar

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', // Exporta um site estático
-  basePath: '',     // Serve diretamente na raiz
-  assetPrefix: '',  // Caminhos absolutos nos assets
+  output: 'export',
 };
 
 export default nextConfig;
