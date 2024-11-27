9f306e769a16dadf2f363d52bdfe0926854254e1
André Castro <andre@visie.com.br>
2024-11-27 14:42:39 -0300

    Configurando o pipeline para ir direto pra raiz

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
+  output: 'export', // Exporta um site estático
+  basePath: '',     // Serve diretamente na raiz
+  assetPrefix: '',  // Caminhos absolutos nos assets
 };
 
 export default nextConfig;
