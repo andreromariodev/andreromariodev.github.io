010dee9a99fbceecb368d4b1a2357a94a83d725b
André Castro <andre@visie.com.br>
2024-11-28 11:16:22 -0300

    Cor da barra

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Cor da barra de endereço no navegador (Chrome/Android) */}
+        <meta name="theme-color" content="#b751d2" />
+        
+        {/* Cor da barra de status do iOS (Safari) */}
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        
+        {/* Configuração para Windows (pinned site) */}
+        <meta name="msapplication-navbutton-color" content="#b751d2" />
+      </Head>
       <body>
         <Main />
         <NextScript />
